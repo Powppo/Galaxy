44e9a3bf38faee78e437cc5bed2ace226816ccce
Powppo <mafrizal575@gmail.com>
2022-10-25 10:38:23 +0700

    fk Adjustment

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <title>Galaxy</title>
@@ -48,3 +47,58 @@
     <!-- Navbar End -->
 </body>
 </html>
+@yield('contactDash')
+
+<body>
+    @yield('contactFooter')
+  <!-- Footer Start -->
+  <div class="footer container-fluid mt-5 py-5 px-sm-3 px-md-5 text-white">
+    <div class="row pt-5">
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-primary mb-4">Get In Touch</h4>
+            <p><i class="fa fa-map-marker-alt mr-2"></i>123 Street, New York, USA</p>
+            <p><i class="fa fa-phone-alt mr-2"></i>+012 345 67890</p>
+            <p><i class="fa fa-envelope mr-2"></i>info@example.com</p>
+            <div class="d-flex justify-content-start mt-4">
+                <a class="btn btn-outline-light rounded-circle text-center mr-2 px-0" style="width: 40px; height: 40px;" href="#"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-light rounded-circle text-center mr-2 px-0" style="width: 40px; height: 40px;" href="#"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light rounded-circle text-center mr-2 px-0" style="width: 40px; height: 40px;" href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-light rounded-circle text-center mr-2 px-0" style="width: 40px; height: 40px;" href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-primary mb-4">Quick Links</h4>
+            <div class="d-flex flex-column justify-content-start">
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Tools and Machine</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Upload</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Exchange Point</a>
+                <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-primary mb-4">Popular Links</h4>
+            <div class="d-flex flex-column justify-content-start">
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>-</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>-</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>-</a>
+                <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>-</a>
+                <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>-</a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-primary mb-4">Opening Hours</h4>
+            <h5 class="text-white">Monday - Friday</h5>
+            <p>8.00 AM - 8.00 PM</p>
+            <h5 class="text-white">Saturday - Sunday</h5>
+            <p>2.00 PM - 8.00 PM</p>
+        </div>
+    </div>
+    <div class="container border-top border-dark pt-5">
+        <p class="m-0 text-center text-white">
+            &copy; <a class="text-white font-weight-bold" href="#">Galaxy</a>. All Rights Reserved.
+        </p>
+    </div>
+</div>
+<!-- Footer End -->
+</body>
